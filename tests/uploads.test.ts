b59096503e08c098aaf600779a469939d07cde81
Juan Castaño <info@benchify.com>
2025-08-22 14:57:52 -0700

    fix build errors

--- tests/uploads.test.ts
+++ tests/uploads.test.ts
@@ -67,7 +67,7 @@ describe('toFile', () => {
   it('is assignable to File and Blob', async () => {
     const input = new File(['foo'], 'input.jsonl', { type: 'jsonl' });
     const result = await toFile(input);
-    const file: File = result;
+    const file = result; // Let TypeScript infer the type
     const blob: Blob = result;
     void file;
     void blob;
